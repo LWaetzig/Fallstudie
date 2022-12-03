a2c430b73e79cabcc1f0f874c47ee54f50553b68
lwaetzig <lucas@waetzig.net>
2022-12-03 17:18:10 +0100

    feat(get_stock_data.py): implement function to request stock data from yFinance

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.get_stock_data import request_stock_data
+
+data = request_stock_data("aapl")
+
+data.keys()
+
--- src/get_stock_data.py
+++ src/get_stock_data.py
@@ -2,41 +2,28 @@ import yfinance as yf
 import plotly.graph_objs as go
 import pandas as pd
 
-class Stock():
 
-    def __init__(self, name):
-        self.name = name
+def request_stock_data(name) -> dict:
+    """request stock data from yahoo finance
+    Returns:
+        dict: dictionary storing important stock information and historical data
+    """
+    interval = "1d"
+    period = "max"
+    
+    data = dict()
+    data["stock_name"] = name
+    
+    stock = yf.Ticker(name)
+    data["data"] = stock.history(interval=interval, period=period)
+    data["info"] = {
+        "sector" : stock.info["sector"],
+        "major_holders" : stock.major_holders,
+        "institutional_holders" : stock.institutional_holders
+        }
+    
+    return data
 
-
-    def request_stock_data(self) -> dict:
-        """request stock data from yahoo finance 
-
-        Returns:
-            dict: dictionary storing important stock information and historical data
-        """
-
-        interval = "1d"
-        period = "max"
-        data = yf.Ticker(self.name).history(interval=interval, period=period)
-        data = data.drop(columns=["Volume" , "Dividends" , "Stock Splits"])
-
-        return data
-
-    def create_visualization(self) -> None:
-        """create visualization using plotly
-        """
-
-
-
-
-aapl = yf.Ticker("AAPL")
-div = aapl.get_actions()
-an = aapl.get_analysis()
-reco = aapl.get_recommendations()
-
-info = aapl.get_calendar()
-
-
-
-news = aapl.get_news()
-news[0]
+def create_visualization(self) -> None:
+    """create visualization using plotly
+    """
