318a6bdb55cdb62f9d7efc8242ce8572bbba458a
lwaetzig <lucas@waetzig.net>
2022-12-14 08:48:50 +0100

    feat(main.py): wip

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from src.get_stock_data import request_stock_data
+import plotly.graph_objects as go
 
 data = request_stock_data("aapl")
 
 data.keys()
+
+fig = go.Figure(data=[
+    go.Candlestick(
+        x=data["data"].index,
+        open=data["data"]["Open"],
+        high=data["data"]["Open"],
+        low=data["data"]["Low"],
+        close=data["data"]["Close"],
+    )
+])
+
+fig.show()
+data
+
+
 
