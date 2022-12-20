c707dd6fef77afce53efec0e105a8609e53b1073
lwaetzig <lucas@waetzig.net>
2022-12-20 15:07:30 +0100

    feat(login.js): fixed an bug

--- assets/add_styles/login.js
+++ assets/add_styles/login.js
@@ -1,9 +1,10 @@
-const loginForm = document.getElementById("login-form")
+const loginForm = document.getElementById("login-field")
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const loginErrorMsg = document.getElementById("error-msg");
 
 loginButton.addEventListener("click", (e) =>{
     e.preventDefault();
+    
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
