7a8491d965876e68655ad839b7447f047dda3448
LWaetzig <lucas@waetzig.net>
2023-01-07 16:40:58 +0100

    feat(views.py): added views for each website page

--- fideo/views.py
+++ fideo/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request=request, template_name="index.html")
+
+def about_us_view(request):
+    return render(request=request , template_name="aboutUs.html")
+
+def fideo_view(request):
+    return render(request=request , template_name="fideo.html")
+
+def impressum_view(request):
+    return render(request=request , template_name="impressum.html")
+
+def login_view(request):
+    return render(request=request , template_name="login.html")
