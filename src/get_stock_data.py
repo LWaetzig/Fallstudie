479ff97614da0f3316355d5b9d2aabca564e4321
Nicho Link <nicholaselink@gmail.com>
2023-01-10 18:54:28 +0100

    Data collection implemented

--- src/get_stock_data.py
+++ src/get_stock_data.py
@@ -1,109 +1,43 @@
-import json
 import os
 
 import pandas as pd
-import plotly.graph_objs as go
 import yfinance as yf
 
+import warnings
 
-def ticker(share_symbol):
-    symbol = yf.Ticker(str(share_symbol))
-    return symbol
+from Finvizpro_copy_edited import web_scraping
 
+warnings.simplefilter("ignore")
 
-def historical_data(shares: list):
-    """function to get all history data for each share in list
+def get_stock_data():
+    """function to get all necessary information and historical share price
+    
+    first run the Finvizpro program"""
 
-    Args:
-        shares (list): list containing share tags as string
-    """
-
+    # define list with seleted shares
+    shares = ['AMZN',"GOOG","BNTX","NKE","AAPL","KO","META","MSFT","NVDA","PYPL","SAP","TSLA"]
+    # create csv file for historical share prices
     if not os.path.exists("shares"):
         os.mkdir("shares")
 
-    for i in shares:
-        history = ticker(i).history(period="5d", actions=False)
-        history.to_csv(f"shares/{i}.csv")
+    spalten = ["tag", "name", "sector", "volatility", "peg_ratio", "betafactor", "histpath"]
+    df = pd.DataFrame(columns=spalten)
 
+    dfcompounds = web_scraping()
 
-def create_data_dict(shares: list) -> dict:
-    """function that takes a list with selected shares and creates a dictionary with important information
-    Args:
-        shares (list): selected shares
+    for i, tag in enumerate(shares):
+        ticker = yf.Ticker(str(tag))
+        history = ticker.history(period="1y", actions=False)
+        history.to_csv(f"shares/{tag}.csv")
+        df.loc[i, "tag"] = tag
+        df.loc[i, "name"] = str(ticker.info["longName"])
+        df.loc[i, "sector"] = str(ticker.info["sector"])
+        df.loc[i, "peg_ratio"] = float(ticker.info["pegRatio"])
+        df.loc[i, "betafactor"] = float(ticker.info["beta"])
+        df.loc[i, "histpath"] = f"shares/{tag}.csv"
+        df.loc[i, "volatility"] = (history['Close'].pct_change().std() * 100).round(2)   
+    df = pd.merge(df, dfcompounds, on='tag', how='left')
+    #print(df)
+    df.to_csv("sharesdata.csv")
 
-    Returns:
-        dict: content (see function above)
-    """
-    share_dict = dict()
-    for i in shares:
-        share_dict.update(
-            {
-                str(i): {
-                    "LongName": str(ticker(i).info["longName"]),
-                    "Sector": str(ticker(i).info["sector"]),
-                    "HistData": f"shares/{i}.csv",
-
-                    # ich hoffe 52WeekChange ist die Volatilität in % und ich bin nicht dumm
-                    # laut ChatGPT bin ich dumm lul, ist nicht das selbe
-                    "volatility": float(ticker(i).info["52WeekChange"]),
-
-                    
-
-                    "peg": float(ticker(i).info["pegRatio"]),
-
-                    "beta": float(ticker(i).info["beta"]),
-                }
-            }
-        )
-
-    return share_dict
-
-df = pd.DataFrame(create_data_dict())
-df.to_csv("sharesdata.csv", index=False)
-
-def wrapper_function() -> None:
-    """wrapper function to create .csv files for historical stock data and .json file with further information about the share"""
-
-    selected_shares = [
-        "ADDDF",
-        "ALIZF",
-        "GOOGL",
-        "AMZN",
-        "AAPL",
-        "BFFAF",
-        "BAYZF",
-        "BNTX",
-        "BAMXF",
-        "KO",
-        "CRZBF",
-        "MBGAF",
-        "DB",
-        "DLAKF",
-        "META",
-        "MSFT",
-        "NKE",
-        "NVDA",
-        "PYPL",
-        "POAHF",
-        "SAPGF",
-        "SMAWF",
-        "DTEGF",
-        "TSLA",
-        "VLKPF",
-    ]
-
-    historical_data(selected_shares)
-    share_dict = create_data_dict(selected_shares)
-
-
-
-def get_stock_data():
-    """function to get all necessary information and historical share price"""
-
-    # define list with seleted shares
-
-    # create csv file for historical share prices
-    
-    
-    # create csv file with information depending on the share
-
+get_stock_data()  
