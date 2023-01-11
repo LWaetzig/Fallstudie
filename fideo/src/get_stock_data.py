313750fc8a60efa1a1440c6069c52510a8c8ff69
Nicho Link <nicholaselink@gmail.com>
2023-01-11 15:30:57 +0100

    Risk_level berechnet

--- fideo/src/get_stock_data.py
+++ fideo/src/get_stock_data.py
@@ -33,18 +33,6 @@ def get_stock_data():
         "TSLA",
     ]
 
-    # columns = [
-    #     "tag",
-    #     "name",
-    #     "sector",
-    #     "volatility",
-    #     "peg_ratio",
-    #     "betafactor",
-    #     "histpath",
-    #     "market_cap",
-    #     "volume",
-    #     "last_close_price"
-    # ]
     df = pd.DataFrame()
 
     # get compund value from nlp algorithm to classify news depending to the share
@@ -58,38 +46,38 @@ def get_stock_data():
         df.loc[i, "tag"] = tag
         df.loc[i, "name"] = str(ticker.info["longName"])
         df.loc[i, "sector"] = str(ticker.info["sector"])
-        df.loc[i, "peg_ratio"] = ticker.info["trailingPegRatio"]
+        df.loc[i, "peg_ratio"] = float(ticker.info["trailingPegRatio"])
         df.loc[i, "betafactor"] = float(ticker.info["beta"])
         df.loc[i, "histpath"] = f"{hist_data_path}/{tag}.csv"
-        df.loc[i, "volatility"] = (history["Close"].pct_change().std() * 100).round(2)
+        vol = (history["Close"].pct_change().std() * (252**0.5)*100).round(2)
+        df.loc[i, "volatility"] = vol
         df.loc[i, "market_cap"] = float(ticker.info["marketCap"])
         df.loc[i, "volume"] = float(ticker.info["volume"])
         df.loc[i, "last_close_price"] = float(history["Close"][-1].round(3))
 
-        # class_peg_ratio = 0
-        # if float(ticker.info["pegRatio"]) > 1:
-        #     class_peg_ratio = -1
-        # elif float(ticker.info["pegRatio"]) < 1:
-        #     class_peg_ratio = 1
-# 
-        # class_betafactor = 0
-        # if float(ticker.info["beta"]) > 1:
-        #     class_peg_ratio = -1
-        # elif float(ticker.info["beta"]) < 1:
-        #     class_peg_ratio = 1
+        class_peg_ratio = 0
+        if float(ticker.info["pegRatio"]) > 2.5:
+            class_peg_ratio = -1
+        elif float(ticker.info["pegRatio"]) < 1.75:
+            class_peg_ratio = 1
 
+        class_betafactor = 0
+        if float(ticker.info["beta"]) > 1.15:
+            class_peg_ratio = -1
+        elif float(ticker.info["beta"]) < 0.95:
+            class_peg_ratio = 1
+        
+        class_volatility = 0
+        if vol > 45:
+            class_volatility = -1
+        elif vol <= 20 :
+            class_volatility = 1
+
+        df.loc[i, "risk_level"] = class_volatility + class_betafactor + class_peg_ratio
 
 
     # merge dataframe containing all necessary information with compund dataframe
     df = pd.merge(df, dfcompounds, on="tag", how="left")
-
-    # "class_volatility"
-    # 0-16 -> 0
-    # 16-45 -> 1
-    # > 45 -> 2
-
-    # "risk_level" = sum(class_volatitily, class_betafactor, class_peg_ratio) 
-    # alternative: mean()
 
     df.to_csv(f"{data_storage_path}/sharesdata.csv")
 
