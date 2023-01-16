6ffe46748c293c7fa8a9085d8ea7afbce49a3bc6
LWaetzig <lucas@waetzig.net>
2023-01-16 23:59:55 +0100

    feat(get_stock_data.py): wip

--- fideo/src/get_stock_data.py
+++ fideo/src/get_stock_data.py
@@ -43,16 +43,13 @@ def get_stock_data():
         history.to_csv(f"{hist_data_path}/{tag}.csv")
 
         # get all information for each share
-        share_name, share_sector = str(ticker.info["longName"]), str(
-            ticker.info["sector"]
-        )
+        share_name = str(ticker.info["longName"])
+        share_sector = str(ticker.info["sector"])
 
-        peg_ratio, beta_factor, market_cap, volume = (
-            float(ticker.info["trailingPegRatio"]),
-            float(ticker.info["beta"]),
-            float(ticker.info["marketCap"]),
-            float(ticker.info["volume"]),
-        )
+        peg_ratio = float(ticker.info["trailingPegRatio"])
+        beta_factor = float(ticker.info["beta"])
+        market_cap = float(ticker.info["marketCap"])
+        volume = float(ticker.info["volume"])
         volatility = (history["Close"].pct_change().std() * (252**0.5) * 100).round(2)
 
         # store information in DataFrame
@@ -67,42 +64,55 @@ def get_stock_data():
         df.loc[i, "last_close_price"] = float(history["Close"][-1].round(3))
         df.loc[i, "histpath"] = f"{hist_data_path}/{tag}.csv"
 
-        # calculation to define risk level for each share
-        # classification: -1 = high risk, 0 = neutral, 1 = low risk
-        class_peg_ratio = 0
-        class_beta_factor = 0
-        class_volatility = 0
-
-        if peg_ratio > 2.5:
-            class_peg_ratio = -1
-        elif peg_ratio < 1.75:
-            class_peg_ratio = 1
-
-        if beta_factor > 1.15:
-            class_beta_factor = -1
-        elif beta_factor < 0.95:
-            class_beta_factor = 1
-
-        if volatility > 45:
-            class_volatility = -1
-        elif volatility <= 20:
-            class_volatility = 1
-
-        df.loc[i, "class_peg_ratio"] = class_peg_ratio
-        df.loc[i, "class_beta_factor"] = class_beta_factor
-        df.loc[i, "class_volatility"] = class_volatility
-
-        # add risk level to DataFrame
-        df.loc[i, "risk_level"] = class_volatility + class_beta_factor + class_peg_ratio
-
     # merge dataframe containing all necessary information with compund dataframe
     df = pd.merge(df, dfcompounds, on="tag", how="left")
 
+    for i , row in df.iterrows():
+
+        # calculation to define risk level for each share
+        # classification: -1 = high risk, 0 = neutral, 1 = low risk
+        # classify each share value
+        class_peg_ratio = 0
+        class_beta_factor = 0
+        class_volatility = 0
+        class_compound = 0
+
+        if row["peg_ratio"] > 2.5:
+            class_peg_ratio = -1
+        elif row["peg_ratio"] < 1.75:
+            class_peg_ratio = 1
+
+        if row["beta_factor"] > 1.15:
+            class_beta_factor = -1
+        elif row["beta_factor"] < 0.95:
+            class_beta_factor = 1
+
+        if row["volatility"] > 45:
+            class_volatility = -1
+        elif row["volatility"] <= 20:
+            class_volatility = 1
+
+        if row["compound"] < -0.05:
+            class_compound = -1
+        elif row["compound"] > 0.05:
+            class_compound = 1
+
+        class_mean = (class_volatility + class_beta_factor + class_peg_ratio + class_compound) / 4
+
+        # caluclate risk level
+        if class_mean <= 0 and class_mean > -1.0:
+            risk_level = 1
+        elif class_mean <= -2/3:
+            risk_level = 2
+        else:
+            risk_level = 0
+
+        # add risk level to DataFrame
+        df.loc[i, "risk_level"] = risk_level
+
     df.to_csv(f"{data_storage_path}/sharesdata.csv")
 
-
-# get_stock_data()
-# df = pd.read_csv("fideo/data/sharesdata.csv")
+# df = pd.read_csv("fideo/data/sharesdata.csv" , index_col=0)
 
 
 def create_small_visualization(file_path: str):
