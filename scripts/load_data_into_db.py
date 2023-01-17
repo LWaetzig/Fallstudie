506f53d695f90d9f3cbfa5c8b8f64eac11b330d9
LWaetzig <lucas@waetzig.net>
2023-01-17 01:03:36 +0100

    feat(load_data_into_db.py): update script to load new content into database

--- scripts/load_data_into_db.py
+++ scripts/load_data_into_db.py
@@ -19,11 +19,11 @@ def run():
                 share_sector=row[3],
                 share_peg_ratio=row[4],
                 share_beta_factor=row[5],
-                share_historical=row[6],
-                share_volatility=row[7],
-                share_market_cap=row[8],
-                share_volume=row[9],
-                last_share_price=row[10],
+                share_volatility=row[6],
+                share_market_cap=row[7],
+                share_volume=row[8],
+                last_share_price=row[9],
+                share_historical=row[10],
                 share_compound=row[11],
                 risk_level=row[12]
 
